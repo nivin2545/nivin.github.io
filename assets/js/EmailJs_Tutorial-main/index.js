2e8ba9cba57305d12d3244438c45d31f77adb9d1
nivin2545 <nivin2545@gmail.com>
2024-03-21 14:52:52 +0400

    Added new files

---
+++ assets/js/EmailJs_Tutorial-main/index.js
@@ -0,0 +1,25 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    number: document.getElementById("number").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_0ezn95u";
+  const templateID = "template_i806bnd";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("number").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
+
